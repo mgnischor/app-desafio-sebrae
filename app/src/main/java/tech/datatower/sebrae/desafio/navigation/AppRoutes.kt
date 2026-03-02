5d9c60509956dae210617cc393c7cb9772d8814a
Miguel Nischor <miguel@nischor.com.br>
2026-03-01 21:41:08 -0400

    Implementação do objeto AppRoutes com constantes para as rotas do aplicativo

---
+++ app/src/main/java/tech/datatower/sebrae/desafio/navigation/AppRoutes.kt
@@ -0,0 +1,14 @@
+package tech.datatower.sebrae.desafio.navigation
+
+object AppRoutes {
+    const val HOME         = "home"
+    const val STUDENTS     = "students"
+    const val COURSES      = "courses"
+    const val CLASSES      = "classes"
+    const val TEACHERS     = "teachers"
+    const val REPORTS      = "reports"
+    const val CERTIFICATES = "certificates"
+    const val CALENDAR     = "calendar"
+    const val SETTINGS     = "settings"
+}
+
