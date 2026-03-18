b0e451469ace2b790e974432d1c9d050b5426d32
Miguel Nischor <miguel@nischor.com.br>
2026-03-17 21:48:02 -0400

    Adiciona documentação para a classe Teacher

--- app/src/main/java/tech/datatower/sebrae/desafio/data/model/Teacher.kt
+++ app/src/main/java/tech/datatower/sebrae/desafio/data/model/Teacher.kt
@@ -2,6 +2,17 @@ package tech.datatower.sebrae.desafio.data.model
 
 import androidx.compose.runtime.Immutable
 
+/**
+ * Representa um instrutor cadastrado no sistema.
+ *
+ * @property id Identificador único do instrutor.
+ * @property name Nome completo do instrutor.
+ * @property email E-mail institucional.
+ * @property specialty Área principal de atuação.
+ * @property activeCourses Quantidade de cursos em que atua atualmente.
+ * @property totalStudents Total de alunos atendidos em suas turmas ativas.
+ * @property rating Avaliação média do instrutor no intervalo de `0f..5f`.
+ */
 @Immutable
 data class Teacher(
     val id: Int,
