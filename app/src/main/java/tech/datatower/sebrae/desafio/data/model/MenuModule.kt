9818ff25a6ca66edaf1f4f88c441b95f6836a91f
Miguel Nischor <miguel@nischor.com.br>
2026-03-17 21:47:44 -0400

    Adiciona documentação para a classe MenuModule

--- app/src/main/java/tech/datatower/sebrae/desafio/data/model/MenuModule.kt
+++ app/src/main/java/tech/datatower/sebrae/desafio/data/model/MenuModule.kt
@@ -3,6 +3,15 @@ package tech.datatower.sebrae.desafio.data.model
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.vector.ImageVector
 
+/**
+ * Define um módulo navegável da tela inicial.
+ *
+ * @property titleRes Recurso de string do título do módulo.
+ * @property descriptionRes Recurso de string da descrição auxiliar.
+ * @property icon Ícone vetorial exibido no cartão do módulo.
+ * @property route Rota de navegação acionada ao clicar no módulo.
+ * @property badgeCount Quantidade opcional de pendências exibida como badge.
+ */
 @Immutable
 data class MenuModule(
     val titleRes: Int,
