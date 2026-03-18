43cb12b57fba45f9a3b57c505f7fde8f4cd6f02a
Miguel Nischor <miguel@nischor.com.br>
2026-03-17 21:47:50 -0400

    Adiciona documentação para a classe RecentActivity

--- app/src/main/java/tech/datatower/sebrae/desafio/data/model/RecentActivity.kt
+++ app/src/main/java/tech/datatower/sebrae/desafio/data/model/RecentActivity.kt
@@ -3,6 +3,14 @@ package tech.datatower.sebrae.desafio.data.model
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.vector.ImageVector
 
+/**
+ * Item de atividade recente apresentado no dashboard.
+ *
+ * @property title Título do evento recente.
+ * @property subtitle Descrição complementar do evento.
+ * @property icon Ícone representativo da atividade.
+ * @property timeLabel Marcador textual de tempo relativo.
+ */
 @Immutable
 data class RecentActivity(
     val title: String,
