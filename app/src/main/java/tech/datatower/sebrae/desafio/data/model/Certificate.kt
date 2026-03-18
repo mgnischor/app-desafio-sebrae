66bfa3354fd0fbe86f780773da290d68480077e1
Miguel Nischor <miguel@nischor.com.br>
2026-03-17 21:47:37 -0400

    Adiciona documentação para a classe Certificate

--- app/src/main/java/tech/datatower/sebrae/desafio/data/model/Certificate.kt
+++ app/src/main/java/tech/datatower/sebrae/desafio/data/model/Certificate.kt
@@ -2,6 +2,16 @@ package tech.datatower.sebrae.desafio.data.model
 
 import androidx.compose.runtime.Immutable
 
+/**
+ * Representa um certificado emitido para conclusão de curso.
+ *
+ * @property id Identificador único do certificado.
+ * @property studentName Nome do aluno certificado.
+ * @property courseName Nome do curso concluído.
+ * @property issuedDate Data de emissão no formato de exibição da interface.
+ * @property hours Carga horária certificada em horas.
+ * @property code Código único de validação do certificado.
+ */
 @Immutable
 data class Certificate(
     val id: Int,
