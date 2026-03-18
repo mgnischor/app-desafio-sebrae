f64a44f773a07f3961da80d9fa68515b463c6d36
Miguel Nischor <miguel@nischor.com.br>
2026-03-17 20:09:22 -0400

    Adição de marcador @Immutable e formatação do arquivo

--- app/src/main/java/tech/datatower/sebrae/desafio/data/model/Student.kt
+++ app/src/main/java/tech/datatower/sebrae/desafio/data/model/Student.kt
@@ -1,14 +1,20 @@
 package tech.datatower.sebrae.desafio.data.model
 
+import androidx.compose.runtime.Immutable
+
+@Immutable
 data class Student(
     val id: Int,
     val name: String,
     val email: String,
     val course: String,
     val enrolledClass: String,
-    val progress: Float,       // 0f..1f
+    val progress: Float, // 0f..1f
     val status: StudentStatus,
 )
 
-enum class StudentStatus { Active, Inactive, Graduated }
-
+enum class StudentStatus {
+  Active,
+  Inactive,
+  Graduated,
+}
