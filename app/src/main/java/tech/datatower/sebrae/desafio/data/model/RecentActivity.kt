a20c3d42c9112159b85f43f18226bf80b35511e7
Miguel Nischor <miguel@nischor.com.br>
2026-03-17 20:09:08 -0400

    Adição de marcador @Immutable e formatação do arquivo

--- app/src/main/java/tech/datatower/sebrae/desafio/data/model/RecentActivity.kt
+++ app/src/main/java/tech/datatower/sebrae/desafio/data/model/RecentActivity.kt
@@ -1,11 +1,12 @@
 package tech.datatower.sebrae.desafio.data.model
 
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.vector.ImageVector
 
+@Immutable
 data class RecentActivity(
     val title: String,
     val subtitle: String,
     val icon: ImageVector,
     val timeLabel: String,
 )
-
