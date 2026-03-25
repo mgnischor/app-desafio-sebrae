23163cf15d1342471779fd3ff25b80a5b750c720
Miguel Nischor <miguel@nischor.com.br>
2026-03-24 21:47:09 -0400

    refactor: Remove comentários desnecessários no SebraeApplication

--- app/src/main/java/tech/datatower/sebrae/desafio/SebraeApplication.kt
+++ app/src/main/java/tech/datatower/sebrae/desafio/SebraeApplication.kt
@@ -3,13 +3,10 @@ package tech.datatower.sebrae.desafio
 import android.app.Application
 import tech.datatower.sebrae.desafio.data.repository.AppGraph
 
-/**
- * Application entrypoint used to pre-warm app dependencies before first UI frame.
- */
+/** Application entrypoint used to pre-warm app dependencies before first UI frame. */
 class SebraeApplication : Application() {
   override fun onCreate() {
     super.onCreate()
     AppGraph.warmUp(this)
   }
 }
-
