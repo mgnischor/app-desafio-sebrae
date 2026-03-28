0b6215d96ee77558e5e7596ab8d7be678e007da0
Miguel Nischor <miguel@nischor.com.br>
2026-03-28 15:03:54 -0400

    deps: adição da biblioteca android library

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.google.devtools.ksp) apply false
-    alias(libs.plugins.hilt) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.android.library) apply false
+  alias(libs.plugins.google.devtools.ksp) apply false
+  alias(libs.plugins.hilt) apply false
+  alias(libs.plugins.kotlin.compose) apply false
 
-    id("com.google.gms.google-services") version "4.4.4" apply false
+  id("com.google.gms.google-services") version "4.4.4" apply false
 }
