4dcb904f440b7675c06673efa66d0f01b5414a74
Miguel Nischor <miguel@nischor.com.br>
2026-03-28 09:22:53 -0400

    feat: adicionar anotação HiltAndroidApp na classe SebraeApplication

--- app/src/main/java/tech/datatower/sebrae/desafio/SebraeApplication.kt
+++ app/src/main/java/tech/datatower/sebrae/desafio/SebraeApplication.kt
@@ -1,9 +1,11 @@
 package tech.datatower.sebrae.desafio
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import tech.datatower.sebrae.desafio.data.repository.AppGraph
 
 /** Application entrypoint used to pre-warm app dependencies before first UI frame. */
+@HiltAndroidApp
 class SebraeApplication : Application() {
   /** Trata o evento de create no contexto da tela atual. */
   override fun onCreate() {
