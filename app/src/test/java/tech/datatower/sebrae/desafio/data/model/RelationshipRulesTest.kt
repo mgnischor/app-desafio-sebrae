4a14ee7f763fa284fa7968e808e1c09809a0778c
Miguel Nischor <miguel@nischor.com.br>
2026-06-10 23:32:41 -0400

    adição do comentário de cabeçalho

--- app/src/test/java/tech/datatower/sebrae/desafio/data/model/RelationshipRulesTest.kt
+++ app/src/test/java/tech/datatower/sebrae/desafio/data/model/RelationshipRulesTest.kt
@@ -1,3 +1,29 @@
+/*
+    Desafio SEBRAE - Gestão Educacional Transformadora
+
+    Arquivo: /app/src/test/java/tech/datatower/sebrae/desafio/data/model/RelationshipRulesTest.kt
+    Descrição: Testes unitários das regras de relacionamento entre curso, turma e aluno.
+    Autor: Miguel Nischor <miguel@nischor.com.br>
+
+    AVISO DE LICENÇA – USO DEMONSTRATIVO
+
+    Este software é propriedade exclusiva de seu(s) autor(es) e está protegido pelas leis de
+    direitos autorais e demais legislações aplicáveis.
+
+    Sua utilização está estritamente limitada para fins demonstrativos no contexto do evento
+    "Prêmio Educador Transformador" do SEBRAE. Qualquer uso fora desse escopo, incluindo, mas
+    não se limitando a, reprodução, distribuição, modificação, engenharia reversa,
+    sublicenciamento, comercialização ou qualquer outra forma de exploração, é expressamente
+    proibido sem autorização prévia e por escrito do(s) detentor(es) dos direitos.
+
+    Este licenciamento não concede quaisquer direitos de propriedade intelectual ao usuário,
+    sendo permitido apenas o acesso e uso temporário para apresentação e avaliação durante o
+    referido evento.
+
+    O descumprimento destes termos poderá resultar em medidas legais cabíveis.
+
+    Todos os direitos reservados.
+*/
 package tech.datatower.sebrae.desafio.data.model
 
 import org.junit.Assert.assertEquals
